209a1d986149d462bd87f3c9e6edb3b3e12cc8dc
ehsan <ehsan.hvd@gmail.com>
2019-09-24 22:08:40 +0330

    custom views

---
+++ xuiframework/src/main/java/com/tpa/xuiframework/view/CustomCheckbox.kt
@@ -0,0 +1,23 @@
+package com.tpa.xuiframework.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.CheckBox
+import com.tpa.xuiframework.R
+import com.tpa.xuiframework.XConfig
+
+class CustomCheckbox(context: Context, val attrs: AttributeSet?, val defStyleAttr: Int) :
+    CheckBox(context, attrs, defStyleAttr) {
+
+    constructor(context: Context) : this(context, null, 0)
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    init {
+        val ta = getContext().obtainStyledAttributes(attrs, R.styleable.CustomButton, defStyleAttr, 0)
+        val fontI = ta.getInt(R.styleable.CustomButton_fontIndex, 0)
+
+        setTypeface(XConfig.typefaces[fontI])
+        ta.recycle()
+    }
+}
---
+++ xuiframework/src/main/java/com/tpa/xuiframework/view/CustomEditText.kt
@@ -0,0 +1,23 @@
+package com.tpa.xuiframework.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.TextView
+import com.tpa.xuiframework.R
+import com.tpa.xuiframework.XConfig
+
+class CustomEditText(context: Context, val attrs: AttributeSet?, val defStyleAttr: Int) :
+    TextView(context, attrs, defStyleAttr) {
+
+    constructor(context: Context) : this(context, null, 0)
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    init {
+        val ta = getContext().obtainStyledAttributes(attrs, R.styleable.CustomTextView, defStyleAttr, 0)
+        val fontI = ta.getInt(R.styleable.CustomTextView_fontIndex, 0)
+
+        setTypeface(XConfig.typefaces[fontI])
+        ta.recycle()
+    }
+}
