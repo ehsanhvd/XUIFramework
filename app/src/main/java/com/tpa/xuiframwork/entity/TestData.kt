040bdc421daf435c30fe3dd12576e69fc990d649
ehsan <ehsan.hvd@gmail.com>
2019-10-03 14:45:50 +0330

    test enum

--- app/src/main/java/com/tpa/xuiframwork/entity/TestData.kt
+++ app/src/main/java/com/tpa/xuiframwork/entity/TestData.kt
@@ -10,4 +10,13 @@ class TestData(name: String, val profile: String) {
     constructor(name: String) : this(name, "") {
 
     }
+
+    enum class TYPE(val DISPLAY_NAME: String,val VALUE: Int) {
+        TYPE1("dispname1", 1),
+        TYPE2("dispname2", 2);
+
+        override fun toString(): String {
+            return this.name
+        }
+    }
 }
