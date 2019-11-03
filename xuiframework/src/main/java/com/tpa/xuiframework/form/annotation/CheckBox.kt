f8d840e768b0b941121435d1b8cbb3dc73e00d15
ehsan <ehsan.hvd@gmail.com>
2019-11-03 13:22:20 +0330

    auto form builder annotations

---
+++ xuiframework/src/main/java/com/tpa/xuiframework/form/annotation/CheckBox.kt
@@ -0,0 +1,11 @@
+package com.tpa.xuiframework.form.annotation
+
+import android.view.inputmethod.EditorInfo
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class EmailInput(
+    val displayName: String = "",
+    val imeOpt: Int = EditorInfo.IME_ACTION_NEXT,
+    val mandatory: Boolean = false
+)
